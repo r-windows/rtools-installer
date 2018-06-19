aab0af35e9850390cba1b6b51deb1ae691e591bb
Jeroen <jeroenooms@gmail.com>
2018-06-20 01:34:04 +0200

    trigger build

--- aliases.sh
+++ aliases.sh
@@ -2,5 +2,5 @@
 # creates his/her own .bashrc/.bash_profile
 
 # --show-control-chars: help showing Korean or accented characters
-alias ls='ls -F --color=auto --show-control-chars'
+alias ls='ls --color=auto --show-control-chars'
 alias ll='ls -l'
