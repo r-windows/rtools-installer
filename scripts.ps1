747339d5c774b4dc59ac0cd9e90034bbaacd8974
Jeroen Ooms <jeroenooms@gmail.com>
2020-08-20 00:44:09 +0200

    Test build

--- scripts.ps1
+++ scripts.ps1
@@ -10,10 +10,9 @@ $RTOOLS_MIRROR = "https://dl.bintray.com/rtools/installer/"
 # $RTOOLS_MIRROR = "https://ftp.opencpu.org/archive/rtools/4.0/"
 
 ### InnoSetup Mirror
-$INNO_MIRROR = "http://jrsoftware.org/download.php/is-unicode.exe?site=2"
-# $INNO_MIRROR = "https://github.com/jrsoftware/issrc/releases/download/is-5_6_1/innosetup-5.6.1-unicode.exe"
-# $INNO_MIRROR = "https://mlaan2.home.xs4all.nl/ispack/innosetup-5.6.1-unicode.exe"
-# $INNO_MIRROR = "http://files.jrsoftware.org/is/5/innosetup-5.6.1-unicode.exe"
+# $INNO_MIRROR = "http://www.jrsoftware.org/download.php/is.exe?site=2"
+### Latest InnoSetup does not support Vista anymore
+$INNO_MIRROR = "http://files.jrsoftware.org/is/6/innosetup-6.0.4.exe"
 
 function CheckExitCode($msg) {
   if ($LastExitCode -ne 0) {
@@ -55,15 +54,15 @@ function InstallRtools {
 }
 
 Function InstallInno {
-  $inno_url = "http://jrsoftware.org/download.php/is.exe?site=2"
+  Write-Host "Downloading InnoSetup from: " + $INNO_MIRROR
+  & "C:\Program Files\Git\mingw64\bin\curl.exe" -s -o ../innosetup.exe -L $INNO_MIRROR
+  CheckExitCode "Failed to download $INNO_MIRROR"
 
-  Progress ("Downloading InnoSetup from: " + $inno_url)
-  & "C:\Program Files\Git\mingw64\bin\curl.exe" -s -o ../innosetup.exe -L $inno_url
-
-  Progress "Installig InnoSetup"
+  Write-Host "Installig InnoSetup..."
   Start-Process -FilePath ..\innosetup.exe -ArgumentList "/ALLUSERS /SILENT" -NoNewWindow -Wait
+  CheckExitCode "Failed to install InnoSetup"
 
-  Progress "InnoSetup installation: Done"
+  Write-Host "InnoSetup installation: Done" -ForegroundColor Green
   Get-ItemProperty "C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 }
 
